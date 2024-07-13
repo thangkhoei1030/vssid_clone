cb2c7296392eed0d9a86d5b35b53df4a3f06f25a
Chien <buichien1703@gmail.com>
2024-07-13 09:01:31 +0700

    look_up_online_ui

---
+++ lib/features/look_up_online/ui/look_up_receive_bhxh_bhyt.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vssid/core/src_core.dart';
+import '../../src_feature.dart';
+
+class LookUpReceiveBhxhBhyt extends StatelessWidget {
+  const LookUpReceiveBhxhBhyt({super.key});
+
+  @override
+  LookUpReceiveBHXHBHYTController get controller =>
+      Get.put(LookUpReceiveBHXHBHYTController());
+  @override
+  Widget build(BuildContext context) {
+    return PageScaffold(
+      showAppBar: true,
+      title: TextBuild(
+        title: LookUpOnlineString.lookUpMemberReceiverBHXH,
+        fontSize: AppDimens.sizeTextLarge,
+      ),
+      showBackButton: true,
+      child: SingleChildScrollView(
+        child: Obx(() => Column(
+          children: [
+            SelectDialog(
+              valueSelected: controller.cityIdSelected.value?.values.first,
+              showListOptionsDialog: controller.selecteCityId,
+              hintText: LookUpOnlineString.selectProvince,
+              lable: LookUpOnlineString.provinceAndCity,
+            ),
+            UtilWidget.sizedBoxPadding,
+            SelectDialog(
+                  valueSelected:
+                      controller.provinceSelected.value?.values.first,
+                  showListOptionsDialog: controller.getProvinceId,
+                  hintText: LookUpOnlineString.selectDistrict,
+                  lable: LookUpOnlineString.district,
+                ),
+                UtilWidget.sizedBoxPadding,
+                CardBuilder(
+                    borderColor: context.onSurfaceColor,
+                    isBorder: true,
+                    radiusModel:
+                        const RadiusModel(radiusAll: AppDimens.radius8),
+                    paddingModel: const PaddingModel(
+                      paddingHorizontal: AppDimens.defaultPadding,
+                      paddingVerical: AppDimens.paddingSmall,
+                    ),
+                    onTap: () => controller.search(),
+                    child: const TextBuild(
+                      title: LookUpOnlineString.search,
+                    )),
+          ],
+        ).paddingAll(AppDimens.defaultPadding)),
+      ),
+    );
+  }
+}
